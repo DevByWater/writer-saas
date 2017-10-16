d187b09e3dc56416d033ffb2d26cc7fe1d1b1ef5
Ellis Garaudy <ellis@Elliss-MacBook-Pro.local>
2017-10-16 16:47:52 -0600

    added quill to editor component

--- src/renderer/components/EditorPage/EditorComponent.vue
+++ src/renderer/components/EditorPage/EditorComponent.vue
@@ -2,16 +2,23 @@
   <div>
     <h1>{{ componentName }}</h1>
     <h2>{{ routeName }}</h2>
+    <router-link to="/">Go back home</router-link>
+    <quill></quill>
   </div>
 </template>
 
 <script>
+import quill from './QuillComponent'
+
 export default {
   data () {
     return {
       componentName: 'Editor',
       routeName: '/editor'
     }
+  },
+  components: {
+    quill
   }
 }
 </script>
---
+++ src/renderer/components/EditorPage/QuillComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <quill-editor v-model="content" >
+
+  </quill-editor>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      name: 'quill',
+      content: 'Welcome to Quill'
+    }
+  }
+}
+</script>
